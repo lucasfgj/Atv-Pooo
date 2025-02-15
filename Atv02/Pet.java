01275b1d8f12197457e6b9482a6e0aa49693c1ad
lucasfgj <lucasfg071421@gmail.com>
2025-02-15 16:47:43 -0300

    Commit 1
    Corrigindo e Organizando Atividade de Poo( Programação Orientada a objeto )

---
+++ Atv02/Pet.java
@@ -0,0 +1,67 @@
+public class Pet {
+    private String nomePet;
+    private int Idade;
+    private String raca;
+    private String porte;
+    private String Alimento;
+    
+    
+    public Animal(String nomePet, int idade, String raca, String porte, String alimento) {
+        this.nomePet = nomePet;
+        this.Idade = idade;
+        this.raca = raca;
+        this.porte = porte;
+        this.Alimento = alimento;
+    }
+
+
+    public String getNomePet() {
+        return nomePet;
+    }
+
+
+    public void setNomePet(String nomePet) {
+        this.nomePet = nomePet;
+    }
+
+
+    public int getIdade() {
+        return Idade;
+    }
+
+
+    public void setIdade(int idade) {
+        Idade = idade;
+    }
+
+
+    public String getRaca() {
+        return raca;
+    }
+
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+
+    public String getPorte() {
+        return porte;
+    }
+
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+
+    public String getAlimento() {
+        return Alimento;
+    }
+
+
+    public void setAlimento(String alimento) {
+        Alimento = alimento;
+    }
+
+}
