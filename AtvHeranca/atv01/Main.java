303192c8562d121cb75df4f02a298fa7b07bd067
Lucas Ferreira Gabriel <121949232+lucasfgj@users.noreply.github.com>
2025-02-22 21:23:27 +0000

    addHeranca

--- AtvHeranca/atv01/Main.java
+++ AtvHeranca/atv01/Main.java
@@ -3,7 +3,7 @@ package AtvHeranca.atv01;
 public class Main {
     public static void main(String[] args) {
        Processador processador = new Processador("AMD", "5600G", "3");
-       Memoria memoria = new Memoria("Asgard", "Vikens", "16GB");
+       Memoria memoria = new Memoria("Asrock", "Vikens", "16GB");
 
        System.out.println();
        System.out.println(processador.toString());
